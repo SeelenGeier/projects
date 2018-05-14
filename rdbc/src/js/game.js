e02bdb6ec7a9dbbdebc44c09e86c5f59470199bc
Philipp Gemind <p.gemind@reply.de>
2018-05-14 17:01:09 +0200

    updated workflows and added phaser starting config

---
+++ rdbc/src/js/game.js
@@ -0,0 +1,10 @@
+let splashScene = new Phaser.Scene('Splash');
+
+let config = {
+    type: Phaser.AUTO,
+    width: 1280,
+    height: 720,
+    scene: splashScene // the starting scene
+};
+
+let game = new Phaser.Game(config);
