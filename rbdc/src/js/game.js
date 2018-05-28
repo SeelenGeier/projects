96b0db4be74084a5001d7d0d943335ec9537d0d6
Philipp Gemind <p.gemind@reply.de>
2018-05-28 11:42:38 +0200

    added first example set of config files and removed ids

--- rbdc/src/js/game.js
+++ rbdc/src/js/game.js
@@ -1,8 +1,6 @@
 /* global Phaser */
 
-//load config
-
-$startingScene = splashScene
+//load configuration files
 
 //load save data and change starting scene accordingly
 
@@ -11,7 +9,7 @@ let config = {
     width: 720,
     height: 1280,
     backgroundColor: '#000000',
-    parent: "RBDC",
+    //game starts with first provided scene (splashScene)
     scene: [splashScene, profileManagementScene, profileOverviewScene, configScene, shopScene, dungeonScene, resultScene]
 };
 
