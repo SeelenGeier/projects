13451b0c673da52ee62a45dddcc307c22cc0e4cc
SeelenGeier <seelengeier@web.de>
2018-05-29 19:58:04 +0200

    Added reload hack for bitmap issue

--- DroSup/js/game.js
+++ DroSup/js/game.js
@@ -1,5 +1,5 @@
 
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render });
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 //define global variables
 var groups = {};
