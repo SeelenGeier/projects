e525e831123d8e6850294d163ff4c0c5ec60905c
Philipp Gemind <p.gemind@reply.de>
2018-07-23 16:51:05 +0200

    reworked tab generation to be more dynamic and added mock data for buyable items

--- rbdc/src/js/scene/shop.js
+++ rbdc/src/js/scene/shop.js
@@ -70,21 +70,24 @@ class shopScene extends Phaser.Scene {
         this.addBuyTabText(x + 50, y + 40);
     }
 
-    openSellTab() {
+    displayTab() {
         let processedItemsCounter = 0;
 
+        // clear all items that are currently displayed on the tab
+        this.clearDisplayedItems();
+
         // go through all items in inventory
-        for (let itemId in saveObject.profiles[saveObject.currentProfile].inventory.items) {
+        for (let itemId in this[1]) {
             // check if max amount of items are already displayed
-            if (this.itemsDisplayed.length >= this.maxItemsDisplayed) {
+            if (this[0].itemsDisplayed.length >= this[0].maxItemsDisplayed) {
                 // abort the loop
                 break;
             }
 
             // check if the offset has been reached (position to start when scrolled down or up)
-            if (processedItemsCounter >= this.itemsOffset) {
+            if (processedItemsCounter >= this[0].itemsOffset) {
                 // display the item in a new row depending on the item position
-                this.displayItemRow(getItem(itemId), this.itemsDisplayed);
+                this[0].displayItemRow(this[1][itemId], this[0].itemsDisplayed);
             }
 
             // increment the counter of processed Items and continue loop
@@ -92,18 +95,24 @@ class shopScene extends Phaser.Scene {
         }
     }
 
-    openBuyTab() {
-
-    }
-
     displayItemRow(item) {
         console.log(item.itemType + '/' + item.itemName + ' (' + item.durability + ')');
+
+        // display image of item on the right side (about 20% of the row)
+        // display item name
+        // display item durability
+        // display damage/mitigation values
+        // display value
+    }
+
+    clearDisplayedItems() {
+
     }
 
     addSellTabButton(x, y) {
         // add sell button
         new Button('buttonSellTab', ['gameicons', 'export.png'], x, y, this);
-        this.buttonSellTab.on('pointerup', this.openSellTab, this);
+        this.buttonSellTab.on('pointerup', this.displayTab, [this, saveObject.profiles[saveObject.currentProfile].inventory.items]);
         this.buttonSellTab.setTint(0xcc0000);
     }
 
@@ -118,13 +127,13 @@ class shopScene extends Phaser.Scene {
 
         // enable text to be clickable as well
         this.textSellTab.setInteractive();
-        this.textSellTab.on('pointerup', this.openSellTab, this);
+        this.textSellTab.on('pointerup', this.displayTab, [this, saveObject.profiles[saveObject.currentProfile].inventory.items]);
     }
 
     addBuyTabButton(x, y) {
         // add buy button
         new Button('buttonBuyTab', ['gameicons', 'import.png'], x, y, this);
-        this.buttonBuyTab.on('pointerup', this.openBuyTab, this);
+        this.buttonBuyTab.on('pointerup', this.displayTab, [this, this.getBuyableItems()]);
         this.buttonBuyTab.setTint(0x00cc00);
     }
 
@@ -139,6 +148,39 @@ class shopScene extends Phaser.Scene {
 
         // enable text to be clickable as well
         this.textBuyTab.setInteractive();
-        this.textBuyTab.on('pointerup', this.openBuyTab, this);
+        this.textBuyTab.on('pointerup', this.displayTab, [this, this.getBuyableItems()]);
+    }
+
+    getBuyableItems() {
+        let items = {};
+
+        // get the list of always available items
+        items = this.addCommonShopItems(items);
+
+        // get the list of rare items that change after each run
+        items = this.addRareShopItems(items);
+
+        // return all items found
+        return items;
+    }
+
+    addCommonShopItems(items) {
+        // TODO: use config to get common items
+        items.common1 = {itemName:'item1', itemType:'type1', durability:'1000'};
+        items.common2 = {itemName:'item2', itemType:'type2', durability:'2000'};
+        items.common3 = {itemName:'item3', itemType:'type3', durability:'3000'};
+        items.common4 = {itemName:'item4', itemType:'type4', durability:'4000'};
+
+        return items;
+    }
+
+    addRareShopItems(items) {
+        // TODO: use saved rare items that have been generated after a run
+        items.rare1 = {itemName:'item1X', itemType:'type1X', durability:'1'};
+        items.rare2 = {itemName:'item2X', itemType:'type2X', durability:'2'};
+        items.rare3 = {itemName:'item3X', itemType:'type3X', durability:'3'};
+        items.rare4 = {itemName:'item4X', itemType:'type4X', durability:'4'};
+
+        return items;
     }
 }
